24308c17500e5c2b269fe10ecc4cb90abb0a8869
Kuek Zheng Yu <kuekzhengyu@gmail.com>
2025-03-10 23:01:51 +0800

    finish Combine and relative connstr

--- Chef/Program.cs
+++ Chef/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +15,9 @@ namespace FoodiePointManagementSystem
         [STAThread]
         static void Main()
         {
+            string gitRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\.."));
+            AppDomain.CurrentDomain.SetData("DataDirectory", gitRoot);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmChef());
